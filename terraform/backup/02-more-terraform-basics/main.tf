b1a74c1d81a49fcd2e2bf7dfb59b8daa77124f33
Innocent607 <68567585+Innocent607@users.noreply.github.com>
2021-10-02 22:29:28 +0200

    mult' users from list

--- terraform/backup/02-more-terraform-basics/main.tf
+++ terraform/backup/02-more-terraform-basics/main.tf
@@ -1,14 +1,24 @@
-variable "iam_user_name_prefix" {
-  type    = string #any, number, bool, list, map, set, object, tuple
-  default = "my_iam_user"
-}
 
 provider "aws" {
-  region  = "us-east-1"
+  region  = "us-east-2"
   //version = "~> 2.46" (No longer necessary)
 }
 
+variable "iam_user_name_prefix" {
+  type    = string #any, number, bool, list, map, set, object, tuple
+  default = "faraway_fresh_iam_user"
+}
+
+variable "names" {
+  default = ["booster", "Inno607", "cityfox"]
+}
+
 resource "aws_iam_user" "my_iam_users" {
-  count = 1
+  count = length(var.names)
+  name  = var.names[count.index]
+}
+
+resource "aws_iam_user" "my_iam_user" {
+  count = 2
   name  = "${var.iam_user_name_prefix}_${count.index}"
 }
